6c7357670a7a150835782d8b05586af7faa289f4
Shub-ev <shubhamg200204@gmail.com>
2026-02-10 19:28:06 +0530

    Create AdminRepository interface in AdminRepository.java

---
+++ src/main/java/com/ug/ug_inventory_management/repositories/AdminRepository.java
@@ -0,0 +1,9 @@
+package com.ug.ug_inventory_management.repositories;
+
+import com.ug.ug_inventory_management.models.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+    Optional<Admin> findById(Long id);
+}
