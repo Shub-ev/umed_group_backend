84023f4b14ef115f38951f37b6a8f7caff3f5018
Shub-ev <shubhamg200204@gmail.com>
2026-02-12 01:48:37 +0530

    Update AdminController.java:Add createAdmin() controller method().

--- src/main/java/com/ug/ug_inventory_management/controllers/AdminController.java
+++ src/main/java/com/ug/ug_inventory_management/controllers/AdminController.java
@@ -1,4 +1,26 @@
 package com.ug.ug_inventory_management.controllers;
 
+import com.ug.ug_inventory_management.models.Admin;
+import com.ug.ug_inventory_management.services.AdminServices;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hkfu")
 public class AdminController {
+
+    private final AdminServices adminServices;
+
+    public AdminController(AdminServices adminServices) {
+        this.adminServices = adminServices;
+    }
+
+    // Create admin controller
+    @PostMapping("/")
+    public ResponseEntity<Admin> createAdmin(@RequestBody Admin admin) {
+        Admin admin_res = adminServices.createAdmin(admin);
+        System.out.println(admin_res);
+
+        return ResponseEntity.ok(admin_res);
+    }
 }
