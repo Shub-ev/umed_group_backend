00f1a71945519170666c728802ba668ee5aace19
Shub-ev <shubhamg200204@gmail.com>
2026-02-12 01:31:12 +0530

    Update AdminServices.java:Update createAdmin() service.

--- src/main/java/com/ug/ug_inventory_management/services/AdminServices.java
+++ src/main/java/com/ug/ug_inventory_management/services/AdminServices.java
@@ -3,19 +3,25 @@ package com.ug.ug_inventory_management.services;
 import com.ug.ug_inventory_management.models.Admin;
 import com.ug.ug_inventory_management.repositories.AdminRepository;
 import org.jspecify.annotations.NonNull;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+// @Service marks this class as SprintBean.
 @Service
 public class AdminServices {
-    @Autowired
-    AdminRepository adminRepository;
 
-    public Admin createAdmin( @NonNull Admin admin){
-        if(admin.getName()==null || admin.getName().trim().isEmpty()){
+    // Dependency injection by Constructor Injection
+    private final AdminRepository adminRepository;
+    public AdminServices(AdminRepository adminRepository) {
+        this.adminRepository = adminRepository;
+    }
+
+    public Admin createAdmin(@NonNull Admin admin){
+        if((admin.getName() == null) || (admin.getName().trim().isEmpty())){
             return null;
         }
-        admin=adminRepository.save(admin);
+        admin = adminRepository.save(admin);
+        // -> Replace this response with DTO (remove password from object)
+        // -> Replace this response with DTO (remove password from object)
         return admin;
-
     }
 }
