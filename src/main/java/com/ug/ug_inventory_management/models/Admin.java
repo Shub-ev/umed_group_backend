e913de5d88f295279d1e3604425d795cd17d0f31
Shub-ev <shubhamg200204@gmail.com>
2026-02-13 01:48:23 +0530

    Add setters(name, password) to Admin.java

--- src/main/java/com/ug/ug_inventory_management/models/Admin.java
+++ src/main/java/com/ug/ug_inventory_management/models/Admin.java
@@ -31,4 +31,7 @@ public class Admin {
     public String getPassword() {
         return password;
     }
+
+    public void setName(String name) { this.name = name; }
+    public void setPassword(String password) { this.password = password; }
 }
