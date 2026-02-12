36fa840f46d6e2f21f67d1997aedb5a5e4430f15
Shub-ev <shubhamg200204@gmail.com>
2026-02-13 00:00:07 +0530

    Remove Setters

--- src/main/java/com/ug/ug_inventory_management/common/dtos/AdminDTO.java
+++ src/main/java/com/ug/ug_inventory_management/common/dtos/AdminDTO.java
@@ -15,12 +15,4 @@ public class AdminDTO {
     public String getName() {
         return name;
     }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
