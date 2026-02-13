77b09f36ed28fafd52a39ce12c3cca616c2495fd
Shub-ev <shubhamg200204@gmail.com>
2026-02-13 20:54:47 +0530

    Add createEmployee() loginEmployee() updateEmployeeUnitName() updateEmployeePassword() method handlers

--- src/main/java/com/ug/ug_inventory_management/controllers/EmployeeController.java
+++ src/main/java/com/ug/ug_inventory_management/controllers/EmployeeController.java
@@ -1,9 +1,50 @@
 package com.ug.ug_inventory_management.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ug.ug_inventory_management.common.dtos.EmployeeDTO;
+import com.ug.ug_inventory_management.common.dtos.EmployeePasswordUpdateDTO;
+import com.ug.ug_inventory_management.models.Employee;
+import com.ug.ug_inventory_management.services.EmployeeServices;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
+    // Dependency injection using constructor injection.
+    private final EmployeeServices employeeServices;
+    public EmployeeController(EmployeeServices employeeServices) {
+        this.employeeServices = employeeServices;
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<EmployeeDTO> createEmployee(@RequestBody Employee employee) {
+        EmployeeDTO employeeDTO = employeeServices.createEmployee(employee);
+        System.out.println(employeeDTO);
+
+        return ResponseEntity.ok(employeeDTO);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<EmployeeDTO> loginEmployee(@RequestBody Employee employee) {
+        EmployeeDTO employeeDTO = employeeServices.loginEmployee(employee);
+        System.out.println(employeeDTO);
+
+        return ResponseEntity.ok(employeeDTO);
+    }
+
+    @PatchMapping("/update/unit_name")
+    public ResponseEntity<EmployeeDTO> updateEmployeeUnitName(@RequestBody Employee employee) {
+        EmployeeDTO employeeDTO = employeeServices.updateEmployeeUnitName(employee);
+        System.out.println(employeeDTO);
+
+        return ResponseEntity.ok(employeeDTO);
+    }
+
+    @PatchMapping("/update/password")
+    public ResponseEntity<EmployeeDTO> updateEmployeePassword(@RequestBody EmployeePasswordUpdateDTO employeePasswordUpdateDTO) {
+        EmployeeDTO employeeDTO = employeeServices.updateEmployeePassword(employeePasswordUpdateDTO);
+        System.out.println(employeeDTO);
+
+        return ResponseEntity.ok(employeeDTO);
+    }
 }
