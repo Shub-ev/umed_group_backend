7004fb7b64aa6fcbb645405d8eff36763cd94f0d
Shub-ev <shubhamg200204@gmail.com>
2026-02-18 20:44:18 +0530

    Create IllegalArgumentException.java

---
+++ src/main/java/com/ug/ug_inventory_management/common/exceptions/IllegalArgumentException.java
@@ -0,0 +1,7 @@
+package com.ug.ug_inventory_management.common.exceptions;
+
+public class IllegalArgumentException extends RuntimeException {
+    public IllegalArgumentException(String message) {
+        super(message);
+    }
+}
