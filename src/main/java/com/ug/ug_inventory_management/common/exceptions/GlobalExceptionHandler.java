a6371db0cebb058b4009601e769a4f3e3afd3a1d
Shub-ev <shubhamg200204@gmail.com>
2026-02-18 20:20:20 +0530

    Update code wrt ErrorResponse object

--- src/main/java/com/ug/ug_inventory_management/common/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/ug/ug_inventory_management/common/exceptions/GlobalExceptionHandler.java
@@ -6,29 +6,25 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(EmployeeNotFoundException.class)
-    public ResponseEntity<Object> handleEmployeeNotFoundException(EmployeeNotFoundException exception) {
-        Map<String, Object> response = new HashMap<>();
-        response.put("timestamp", LocalDateTime.now());
-        response.put("message", exception.getMessage());
-        response.put("status", HttpStatus.NOT_FOUND);
-
-        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorResponse> handleEmployeeNotFoundException(EmployeeNotFoundException exception) {
+        return new ResponseEntity<>(new ErrorResponse(
+                LocalDateTime.now(),
+                exception.getMessage(),
+                HttpStatus.NOT_FOUND.value()
+        ), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(AdminNotFoundException.class)
-    public ResponseEntity<Object> handleAdminNotFoundException(AdminNotFoundException exception) {
-        Map<String, Object> response = new HashMap<>();
-        response.put("timestamp", LocalDateTime.now());
-        response.put("message", exception.getMessage());
-        response.put("status", HttpStatus.NOT_FOUND);
-
-        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorResponse> handleAdminNotFoundException(AdminNotFoundException exception) {
+        return new ResponseEntity<>(new ErrorResponse(
+                LocalDateTime.now(),
+                exception.getMessage(),
+                HttpStatus.NOT_FOUND.value()
+        ), HttpStatus.NOT_FOUND);
     }
 }
