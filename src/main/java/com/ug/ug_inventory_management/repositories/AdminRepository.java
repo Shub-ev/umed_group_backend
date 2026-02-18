2ac7baf88c9eb944fae5e8f2a61ffd940fbf5061
Shub-ev <shubhamg200204@gmail.com>
2026-02-18 17:31:09 +0530

    Add deleteById() method to Repository

--- src/main/java/com/ug/ug_inventory_management/repositories/AdminRepository.java
+++ src/main/java/com/ug/ug_inventory_management/repositories/AdminRepository.java
@@ -6,4 +6,5 @@ import java.util.Optional;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
     Optional<Admin> findByName(String name);
+    Optional<Admin> deleteByName(String name);
 }
