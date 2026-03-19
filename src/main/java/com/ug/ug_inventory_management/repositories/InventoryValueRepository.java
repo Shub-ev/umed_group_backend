16785320f3892c0cf61e7d03fed08ad0e2838c7b
shivsharanprakash <shivsharanp49@gmail.com>
2026-03-19 16:11:57 +0530

    repositories for InventoryValues for dynamic schema storage

--- src/main/java/com/ug/ug_inventory_management/repositories/InventoryValueRepository.java
+++ src/main/java/com/ug/ug_inventory_management/repositories/InventoryValueRepository.java
@@ -1,4 +1,10 @@
 package com.ug.ug_inventory_management.repositories;
 
-public class InventoryRepository {
+import com.ug.ug_inventory_management.models.InventoryValue;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InventoryValueRepository extends JpaRepository<InventoryValue, Long> {
+    List<InventoryValue> findByRecordId(Long recordId);
 }
