e4d3f3d1cd51dc0c1d6e35504e2ff33ec3b7879c
shivsharanprakash <shivsharanp49@gmail.com>
2026-03-19 16:14:30 +0530

    TemplateRequest for creating dynamic templates

--- src/main/java/com/ug/ug_inventory_management/common/dtos/InventoryRequest.java
+++ src/main/java/com/ug/ug_inventory_management/common/dtos/InventoryRequest.java
@@ -1,4 +1,13 @@
 package com.ug.ug_inventory_management.common.dtos;
-
+import java.util.Map;
 public class InventoryRequest {
+    private Long templateId;
+    private Long unitId;
+    private Map<String, String> values;
+
+    public Long getTemplateId() { return templateId; }
+
+    public Long getUnitId() { return unitId; }
+
+    public Map<String, String> getValues() { return values; }
 }
