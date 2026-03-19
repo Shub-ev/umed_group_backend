6a317ed86fd200a4bf95d35b0fd95b07a8bfa259
shivsharanprakash <shivsharanp49@gmail.com>
2026-03-19 16:02:00 +0530

    Add TemplateField to define dynamic columns with types

--- src/main/java/com/ug/ug_inventory_management/models/TemplateField.java
+++ src/main/java/com/ug/ug_inventory_management/models/TemplateField.java
@@ -1,4 +1,33 @@
 package com.ug.ug_inventory_management.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "template_fields")
 public class TemplateField {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long templateId;
+
+    private String fieldName;
+
+    @Enumerated(EnumType.STRING)
+    private FieldType fieldType;
+
+    public Long getId() { return id; }
+
+    public Long getTemplateId() { return templateId; }
+
+    public void setTemplateId(Long templateId) { this.templateId = templateId; }
+
+    public String getFieldName() { return fieldName; }
+
+    public void setFieldName(String fieldName) { this.fieldName = fieldName; }
+
+    public FieldType getFieldType() { return fieldType; }
+
+    public void setFieldType(FieldType fieldType) { this.fieldType = fieldType; }
 }
