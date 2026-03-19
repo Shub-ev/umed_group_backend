3681994be136304a4de8fe6e9a654ceb9b890692
shivsharanprakash <shivsharanp49@gmail.com>
2026-03-19 16:09:50 +0530

    repositories for Template, TemplateField for dynamic schema storage

--- src/main/java/com/ug/ug_inventory_management/repositories/TemplateFieldRepository.java
+++ src/main/java/com/ug/ug_inventory_management/repositories/TemplateFieldRepository.java
@@ -1,4 +1,10 @@
 package com.ug.ug_inventory_management.repositories;
 
-public class TemplateFieldRepository {
+import com.ug.ug_inventory_management.models.TemplateField;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TemplateFieldRepository extends JpaRepository<TemplateField, Long> {
+    List<TemplateField> findByTemplateId(Long templateId);
 }
