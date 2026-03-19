4951d8cdc915d393ea607a86418536c19484fa15
shivsharanprakash <shivsharanp49@gmail.com>
2026-03-19 16:18:01 +0530

    add logic to create template with dynamic fileds ,validate duplicate template names ,maps field type using enum ,stores metadata instead of storing entire DB

--- src/main/java/com/ug/ug_inventory_management/services/TemplateService.java
+++ src/main/java/com/ug/ug_inventory_management/services/TemplateService.java
@@ -1,4 +1,41 @@
 package com.ug.ug_inventory_management.services;
 
+import com.ug.ug_inventory_management.common.dtos.*;
+import com.ug.ug_inventory_management.models.*;
+import com.ug.ug_inventory_management.repositories.*;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TemplateService {
+
+    private final TemplateRepository templateRepo;
+    private final TemplateFieldRepository fieldRepo;
+
+    public TemplateService(TemplateRepository t, TemplateFieldRepository f) {
+        this.templateRepo = t;
+        this.fieldRepo = f;
+    }
+
+    @Transactional
+    public void createTemplate(TemplateRequest request) {
+
+        if(templateRepo.existsByTemplateName(request.getTemplateName())) {
+            throw new RuntimeException("Template already exists");
+        }
+
+        Template template = new Template();
+        template.setTemplateName(request.getTemplateName());
+        templateRepo.save(template);
+
+        for(FieldRequest f : request.getFields()) {
+
+            TemplateField field = new TemplateField();
+            field.setTemplateId(template.getId());
+            field.setFieldName(f.getName());
+            field.setFieldType(FieldType.valueOf(f.getType()));
+
+            fieldRepo.save(field);
+        }
+    }
 }
