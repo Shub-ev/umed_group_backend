70892cd9f7608cffcf3bb84304f137c9bc4ffc4a
shivsharanprakash <shivsharanp49@gmail.com>
2026-03-19 16:21:20 +0530

    for creating and managing the templates

--- src/main/java/com/ug/ug_inventory_management/controllers/TemplateController.java
+++ src/main/java/com/ug/ug_inventory_management/controllers/TemplateController.java
@@ -1,4 +1,22 @@
 package com.ug.ug_inventory_management.controllers;
 
+import com.ug.ug_inventory_management.common.dtos.TemplateRequest;
+import com.ug.ug_inventory_management.services.TemplateService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/templates")
 public class TemplateController {
+
+    private final TemplateService service;
+
+    public TemplateController(TemplateService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public String createTemplate(@RequestBody TemplateRequest request) {
+        service.createTemplate(request);
+        return "Template Created";
+    }
 }
