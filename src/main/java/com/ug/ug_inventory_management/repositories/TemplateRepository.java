ea5254833993adf7855cf68f97bdcaf37651fdab
shivsharanprakash <shivsharanp49@gmail.com>
2026-03-19 16:08:01 +0530

    Add repositories for Template, TemplateField,for Added repositories for Template, TemplateField

--- src/main/java/com/ug/ug_inventory_management/repositories/TemplateRepository.java
+++ src/main/java/com/ug/ug_inventory_management/repositories/TemplateRepository.java
@@ -1,4 +1,8 @@
 package com.ug.ug_inventory_management.repositories;
 
-public class TemplateRepository {
+import com.ug.ug_inventory_management.models.Template;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TemplateRepository extends JpaRepository<Template, Long> {
+    boolean existsByTemplateName(String templateName);
 }
