f365a1b9aa3dc231363edde1f73dacf6303d32f6
Shub-ev <shubhamg200204@gmail.com>
2026-03-24 01:19:49 +0530

    Add springDoc Annotations

--- src/main/java/com/ug/ug_inventory_management/common/dtos/AdminNameUpdateDTO.java
+++ src/main/java/com/ug/ug_inventory_management/common/dtos/AdminNameUpdateDTO.java
@@ -1,9 +1,20 @@
 package com.ug.ug_inventory_management.common.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "DTO for updating admin name.")
 public class AdminNameUpdateDTO {
+
+    @Schema(description = "Admin id", example = "1")
     private Long id;
+
+    @Schema(description = "Admin old name", example = "John Deo")
     private String oldName;
+
+    @Schema(description = "Admin new name", example = "Deo John")
     private String newName;
+
+    @Schema(hidden = true)
     private String password;
 
     public AdminNameUpdateDTO(Long id, String oldName, String newName, String password) {
