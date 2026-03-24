8215161207b7eea11772cd5fe3d78ad69b79567f
Shub-ev <shubhamg200204@gmail.com>
2026-03-24 20:43:38 +0530

    Update requestMatchers for /inventory api

--- src/main/java/com/ug/ug_inventory_management/config/SecurityConfig.java
+++ src/main/java/com/ug/ug_inventory_management/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/**").permitAll()
+                        .requestMatchers("/inventory/**").permitAll()
                         .requestMatchers("/employee/**").permitAll()
                         .requestMatchers("/hkfu/**").permitAll()
                         .requestMatchers("/swagger-ui/**").permitAll()
