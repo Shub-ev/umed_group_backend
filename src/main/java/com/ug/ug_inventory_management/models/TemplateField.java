6e4db3c81b6b1509436660f3918a317463801ce0
Shub-ev <shubhamg200204@gmail.com>
2026-03-24 12:56:00 +0530

    Update with constructors and comments

--- src/main/java/com/ug/ug_inventory_management/models/TemplateField.java
+++ src/main/java/com/ug/ug_inventory_management/models/TemplateField.java
@@ -3,6 +3,11 @@ package com.ug.ug_inventory_management.models;
 import com.ug.ug_inventory_management.enums.FieldType;
 import jakarta.persistence.*;
 
+/*
+ * Template Fields
+ * This creates pool of fields for different templates.
+ * Each field acts as column of table.
+ */
 @Entity
 @Table(name = "template_fields")
 public class TemplateField {
@@ -18,17 +23,27 @@ public class TemplateField {
     @Enumerated(EnumType.STRING)
     private FieldType fieldType;
 
+
+    public TemplateField() {
+    }
+
+    public TemplateField(Long templateId, String fieldName, FieldType fieldType) {
+        this.templateId = templateId;
+        this.fieldName = fieldName;
+        this.fieldType = fieldType;
+    }
+
     public Long getId() { return id; }
 
     public Long getTemplateId() { return templateId; }
 
-    public void setTemplateId(Long templateId) { this.templateId = templateId; }
-
     public String getFieldName() { return fieldName; }
 
-    public void setFieldName(String fieldName) { this.fieldName = fieldName; }
-
     public FieldType getFieldType() { return fieldType; }
+
+    public void setTemplateId(Long templateId) { this.templateId = templateId; }
+
+    public void setFieldName(String fieldName) { this.fieldName = fieldName; }
 
     public void setFieldType(FieldType fieldType) { this.fieldType = fieldType; }
 }
