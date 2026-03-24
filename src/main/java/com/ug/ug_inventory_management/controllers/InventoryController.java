f72fb03c55264f940525afe770fa31446318eeed
shivsharanprakash <shivsharanp49@gmail.com>
2026-03-24 20:15:34 +0530

    add endpoints to get admin summary view

--- src/main/java/com/ug/ug_inventory_management/controllers/InventoryController.java
+++ src/main/java/com/ug/ug_inventory_management/controllers/InventoryController.java
@@ -1,27 +1,7 @@
-//package com.ug.ug_inventory_management.controllers;
-//import com.ug.ug_inventory_management.services.InventoryService;
-//import com.ug.ug_inventory_management.common.dtos.InventoryRequest;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequestMapping("/api/inventory")
-//public class InventoryController {
-//
-//    private final InventoryService service;
-//
-//    public InventoryController(InventoryService service) {
-//        this.service = service;
-//    }
-//
-//    @PostMapping("/")
-//    public String addInventory(@RequestBody InventoryRequest request) {
-//        service.saveRecord(request);
-//        return "Inventory Saved";
-//    }
-//}
+
 
 package com.ug.ug_inventory_management.controllers;
-
+import java.util.*;
 import com.ug.ug_inventory_management.services.InventoryService;
 import com.ug.ug_inventory_management.common.dtos.InventoryRequest;
 import org.springframework.web.bind.annotation.*;
@@ -43,12 +23,17 @@ public class InventoryController {
         return "Inventory Saved";
     }
 
-    // ✅ GET INVENTORY (IMPORTANT)
-    @GetMapping("/{templateId}/{unitId}")
-    public Object getInventory(
-            @PathVariable Long templateId,
-            @PathVariable Long unitId) {
 
+
+
+    @GetMapping("/{templateId}/{unitId}")
+    public List<Map<String, String>> getInventory(@PathVariable Long templateId,
+                                                  @PathVariable Long unitId) {
         return service.getInventory(templateId, unitId);
+    }
+
+    @GetMapping("/summary/{templateId}")
+    public List<Map<String, String>> getSummary(@PathVariable Long templateId) {
+        return service.getInventorySummary(templateId);
     }
 }
