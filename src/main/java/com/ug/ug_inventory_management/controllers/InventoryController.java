7141849a0cc7c1ae329902f14b03397cb8406f1b
Shub-ev <shubhamg200204@gmail.com>
2026-03-24 20:44:27 +0530

    Update old DTOs with new renamed ones

--- src/main/java/com/ug/ug_inventory_management/controllers/InventoryController.java
+++ src/main/java/com/ug/ug_inventory_management/controllers/InventoryController.java
@@ -1,33 +1,11 @@
-//package com.ug.ug_inventory_management.controllers;
-//import com.ug.ug_inventory_management.services.InventoryService;
-//import com.ug.ug_inventory_management.common.dtos.InventoryRequest;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequestMapping("/api/inventory")
-//public class InventoryController {
-//
-//    private final InventoryService service;
-//
-//    public InventoryController(InventoryService service) {
-//        this.service = service;
-//    }
-//
-//    @PostMapping("/")
-//    public String addInventory(@RequestBody InventoryRequest request) {
-//        service.saveRecord(request);
-//        return "Inventory Saved";
-//    }
-//}
-
 package com.ug.ug_inventory_management.controllers;
 
 import com.ug.ug_inventory_management.services.InventoryService;
-import com.ug.ug_inventory_management.common.dtos.InventoryRequest;
+import com.ug.ug_inventory_management.common.dtos.CreateInventoryRecordDTO;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/inventory")
+@RequestMapping("/inventory")
 public class InventoryController {
 
     private final InventoryService service;
@@ -38,7 +16,7 @@ public class InventoryController {
 
     // ✅ CREATE INVENTORY
     @PostMapping
-    public String addInventory(@RequestBody InventoryRequest request) {
+    public String addInventory(@RequestBody CreateInventoryRecordDTO request) {
         service.saveRecord(request);
         return "Inventory Saved";
     }
@@ -47,8 +25,8 @@ public class InventoryController {
     @GetMapping("/{templateId}/{unitId}")
     public Object getInventory(
             @PathVariable Long templateId,
-            @PathVariable Long unitId) {
-
-        return service.getInventory(templateId, unitId);
+            @PathVariable String unitName)
+    {
+        return service.getInventory(templateId, unitName);
     }
 }
