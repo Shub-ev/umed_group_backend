7976ede3cda21469959780550c91355ebd23276c
shivsharanprakash <shivsharanp49@gmail.com>
2026-03-24 20:12:07 +0530

    add method to fetch records by template Id

--- src/main/java/com/ug/ug_inventory_management/repositories/InventoryRecordRepository.java
+++ src/main/java/com/ug/ug_inventory_management/repositories/InventoryRecordRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+
 public interface InventoryRecordRepository extends JpaRepository<InventoryRecord, Long> {
     List<InventoryRecord> findByTemplateIdAndUnitId(Long templateId, Long unitId);
+    List<InventoryRecord> findByTemplateId(Long templateId);
 }
