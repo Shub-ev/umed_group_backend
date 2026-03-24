8dff3c50b0032b98bfefe97a07025feb66af2fbb
Shub-ev <shubhamg200204@gmail.com>
2026-03-24 12:46:20 +0530

    Update with comments and constructors

--- src/main/java/com/ug/ug_inventory_management/models/Template.java
+++ src/main/java/com/ug/ug_inventory_management/models/Template.java
@@ -2,6 +2,15 @@ package com.ug.ug_inventory_management.models;
 
 import jakarta.persistence.*;
 
+/*
+ * Template Entity
+ * Template acts as the blueprint for table.
+ * IMP: Creating dynamic tables as per client request is not optimal and secure,
+ *      instead we create data pool and based on the template we retrieve the data.
+ *
+ * To create any table we create corresponding template of that table.
+ * Template have corresponding template fields which acts as table columns.
+ */
 @Entity
 @Table(name = "templates")
 public class Template {
@@ -12,6 +21,13 @@ public class Template {
 
     @Column(unique = true)
     private String templateName;
+
+    public Template() {
+    }
+
+    public Template(String templateName) {
+        this.templateName = templateName;
+    }
 
     public Long getId() { return id; }
 
