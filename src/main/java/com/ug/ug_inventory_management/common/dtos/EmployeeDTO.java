f94ef67774d79318453d64099cecd63ffb5ae93d
shivsharanprakash <shivsharanp49@gmail.com>
2026-03-28 18:58:05 +0530

    add names for eid and unit_name for eid and unit_name

--- src/main/java/com/ug/ug_inventory_management/common/dtos/EmployeeDTO.java
+++ src/main/java/com/ug/ug_inventory_management/common/dtos/EmployeeDTO.java
@@ -7,13 +7,13 @@ import java.time.LocalDate;
 @Schema(description = "DTO for responding employee data as response")
 public class EmployeeDTO {
 
-    @Schema(description = "Employee ID")
+    @Schema(name="eid",description = "Employee ID")
     private Long eid;
 
-    @Schema(description = "Employee working unit name")
+    @Schema(name="unit_name",description = "Employee working unit name")
     private String unit_name;
 
-    @Schema(description = "Employee allocation data")
+    @Schema(name="allocation",description = "Employee allocation data")
     private LocalDate allocation;
 
     public EmployeeDTO(Long eid, String unit_name, LocalDate allocation) {
