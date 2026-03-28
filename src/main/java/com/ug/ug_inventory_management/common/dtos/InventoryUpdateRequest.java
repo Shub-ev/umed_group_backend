d87ca5a4ba3005f1b604de30eb083774f259998a
shivsharanprakash <shivsharanp49@gmail.com>
2026-03-28 19:00:06 +0530

    create InventoryUpdateRequest.java to carry inventory update data

--- src/main/java/com/ug/ug_inventory_management/common/dtos/InventoryUpdateRequest.java
+++ src/main/java/com/ug/ug_inventory_management/common/dtos/InventoryUpdateRequest.java
@@ -1,4 +1,52 @@
 package com.ug.ug_inventory_management.common.dtos;
 
 public class InventoryUpdateRequest {
+
+    private Long templateId;
+    private Long unitId;
+    private Long fieldId;
+    private int changeQty;
+    private String action; // INWARD / OUTWARD
+
+    // Getters & Setters
+
+    public Long getTemplateId() {
+        return templateId;
+    }
+
+    public Long getUnitId() {
+        return unitId;
+    }
+
+    public Long getFieldId() {
+        return fieldId;
+    }
+
+    public int getChangeQty() {
+        return changeQty;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setTemplateId(Long templateId) {
+        this.templateId = templateId;
+    }
+
+    public void setUnitId(Long unitId) {
+        this.unitId = unitId;
+    }
+
+    public void setFieldId(Long fieldId) {
+        this.fieldId = fieldId;
+    }
+
+    public void setChangeQty(int changeQty) {
+        this.changeQty = changeQty;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
 }
