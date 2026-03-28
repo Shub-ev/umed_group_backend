1012c6dff0c2907d3feae90fef39d5eb066e77cc
Shub-ev <shubhamg200204@gmail.com>
2026-03-29 01:46:36 +0530

    comment out some code

--- src/main/java/com/ug/ug_inventory_management/repositories/InventoryValueRepository.java
+++ src/main/java/com/ug/ug_inventory_management/repositories/InventoryValueRepository.java
@@ -7,18 +7,20 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface InventoryValueRepository extends JpaRepository<InventoryValue, Long> {
-    List<InventoryValue> findByRecordId(Long recordId);
 
+//    #### We have added table joining
 
-    @Query("""
-SELECT v FROM InventoryValue v
-JOIN InventoryRecord r ON v.recordId = r.id
-JOIN TemplateField f ON v.fieldId = f.id
-WHERE r.templateId = :templateId
-  AND r.unitId = :unitId
-  AND LOWER(f.fieldName) LIKE LOWER(CONCAT('%', :fieldName, '%'))
-""")
-    List<InventoryValue> findFieldByName(Long templateId, Long unitId, String fieldName);
+//    List<InventoryValue> findByRecordId(Long recordId);
+
+//    @Query("""
+//SELECT v FROM InventoryValue v
+//JOIN InventoryRecord r ON v.recordId = r.id
+//JOIN TemplateField f ON v.fieldId = f.id
+//WHERE r.templateId = :templateId
+//  AND r.unitId = :unitId
+//  AND LOWER(f.fieldName) LIKE LOWER(CONCAT('%', :fieldName, '%'))
+//""")
+//    List<InventoryValue> findFieldByName(Long templateId, Long unitId, String fieldName);
 
 
     List<InventoryValue> findByInventoryRecord_Id(Long recordId);
