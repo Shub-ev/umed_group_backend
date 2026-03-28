5c62e045bd3bd811f07cf0ded7f7b45e07bfd4f0
shivsharanprakash <shivsharanp49@gmail.com>
2026-03-28 19:03:37 +0530

    Update InventoryValueRepository.java ,Fetches inventory values by joining related tables and filtering by templateId, unitId, and a case-insensitive partial match of field name

--- src/main/java/com/ug/ug_inventory_management/repositories/InventoryValueRepository.java
+++ src/main/java/com/ug/ug_inventory_management/repositories/InventoryValueRepository.java
@@ -2,9 +2,29 @@ package com.ug.ug_inventory_management.repositories;
 
 import com.ug.ug_inventory_management.models.InventoryValue;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface InventoryValueRepository extends JpaRepository<InventoryValue, Long> {
     List<InventoryValue> findByRecordId(Long recordId);
+
+
+    @Query("""
+SELECT v FROM InventoryValue v
+JOIN InventoryRecord r ON v.recordId = r.id
+JOIN TemplateField f ON v.fieldId = f.id
+WHERE r.templateId = :templateId
+  AND r.unitId = :unitId
+  AND LOWER(f.fieldName) LIKE LOWER(CONCAT('%', :fieldName, '%'))
+""")
+    List<InventoryValue> findFieldByName(Long templateId, Long unitId, String fieldName);
+
+
 }
+
+
+
+
+
+
