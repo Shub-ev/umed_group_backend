014837ccfeca676765cc6fa7b36f0c189a7b1269
Shub-ev <shubhamg200204@gmail.com>
2026-03-29 21:44:14 +0530

    Update with new logs and DTOS

--- src/main/java/com/ug/ug_inventory_management/controllers/EmployeeController.java
+++ src/main/java/com/ug/ug_inventory_management/controllers/EmployeeController.java
@@ -1,9 +1,11 @@
 package com.ug.ug_inventory_management.controllers;
 
-import com.ug.ug_inventory_management.common.dtos.EmployeeDTO;
-import com.ug.ug_inventory_management.common.dtos.EmployeePasswordUpdateDTO;
-import com.ug.ug_inventory_management.models.Employee;
+import com.ug.ug_inventory_management.common.dtos.Admin.AdminDTO;
+import com.ug.ug_inventory_management.common.dtos.Admin.AdminResponseDTO;
+import com.ug.ug_inventory_management.common.dtos.Employee.*;
 import com.ug.ug_inventory_management.services.EmployeeServices;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +15,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
+
+    private static final Logger log = LoggerFactory.getLogger(EmployeeController.class);
+
     // Dependency injection using constructor injection.
     private final EmployeeServices employeeServices;
     public EmployeeController(EmployeeServices employeeServices) {
@@ -20,45 +25,63 @@ public class EmployeeController {
     }
 
     @PostMapping("/")
-    public ResponseEntity<EmployeeDTO> createEmployee(@RequestBody Employee employee) {
-        EmployeeDTO employeeDTO = employeeServices.createEmployee(employee);
-        System.out.println(employeeDTO);
+    public ResponseEntity<ResponseEmployeeDTO> createEmployee(@RequestBody CreateEmployeeDTO createEmployeeDTO) {
+        log.info("Creating employee with Id: {}", createEmployeeDTO.getEId());
+        ResponseEmployeeDTO responseEmployeeDTO = employeeServices.createEmployee(createEmployeeDTO);
+        log.info("Created Employee: {}", responseEmployeeDTO);
 
-        return ResponseEntity.ok(employeeDTO);
+        return ResponseEntity.ok(responseEmployeeDTO);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<EmployeeDTO> loginEmployee(@RequestBody Employee employee) {
-        EmployeeDTO employeeDTO = employeeServices.loginEmployee(employee);
-        System.out.println(employeeDTO);
+    public ResponseEntity<ResponseEmployeeDTO> loginEmployee(@RequestBody LoginEmployeeDTO loginEmployeeDTO) {
+        log.info("Login with employee ID: {}", loginEmployeeDTO.getEId());
+        ResponseEmployeeDTO responseEmployeeDTO = employeeServices.loginEmployee(loginEmployeeDTO);
+        log.info("Login process completed, response: {}", responseEmployeeDTO);
 
-        return ResponseEntity.ok(employeeDTO);
+        return ResponseEntity.ok(responseEmployeeDTO);
     }
 
     @PatchMapping("/update/unit_name")
-    public ResponseEntity<EmployeeDTO> updateEmployeeUnitName(@RequestBody Employee employee) {
-        EmployeeDTO employeeDTO = employeeServices.updateEmployeeUnitName(employee);
-        System.out.println(employeeDTO);
+    public ResponseEntity<ResponseEmployeeDTO> updateEmployeeUnitName(@RequestBody EmployeeUnitNameUpdateDTO employeeUnitNameUpdateDTO) {
+        log.info("Updating unit name for employee: {}", employeeUnitNameUpdateDTO.geteId());
+        ResponseEmployeeDTO responseEmployeeDTO = employeeServices.updateEmployeeUnitName(employeeUnitNameUpdateDTO);
+        log.info("Unit name updated successfully for employee: {}", employeeUnitNameUpdateDTO.geteId());
+
+        return ResponseEntity.ok(responseEmployeeDTO);
+    }
+
+    @PatchMapping("/update/password")
+    public ResponseEntity<ResponseEmployeeDTO> updateEmployeePassword(@RequestBody EmployeePasswordUpdateDTO employeePasswordUpdateDTO) {
+        log.info("Updating password for employee ID: {}", employeePasswordUpdateDTO.geteId());
+        ResponseEmployeeDTO employeeDTO = employeeServices.updateEmployeePassword(employeePasswordUpdateDTO);
+        log.info("Password updated successfully for employee: {}", employeePasswordUpdateDTO.geteId());
 
         return ResponseEntity.ok(employeeDTO);
     }
 
-    @PatchMapping("/update/password")
-    public ResponseEntity<EmployeeDTO> updateEmployeePassword(@RequestBody EmployeePasswordUpdateDTO employeePasswordUpdateDTO) {
-        EmployeeDTO employeeDTO = employeeServices.updateEmployeePassword(employeePasswordUpdateDTO);
-        System.out.println(employeeDTO);
+    @DeleteMapping("/delete")
+    public ResponseEntity<ResponseEmployeeDTO> deleteEmployee(@RequestBody LoginEmployeeDTO loginEmployeeDTO) {
+        log.info("Deleting employee with ID: {}", loginEmployeeDTO.getEId());
+        ResponseEmployeeDTO responseEmployeeDTO = employeeServices.deleteEmployee(loginEmployeeDTO);
+        log.info("Employee deleted successfully with ID: {}", loginEmployeeDTO.getEId());
 
-        return ResponseEntity.ok(employeeDTO);
+        return ResponseEntity.ok(responseEmployeeDTO);
     }
 
     @GetMapping("/count")
     public Long getEmployeeCount() {
-        return employeeServices.employeeCount();
+        log.info("Fetching employee count");
+        Long employeeCount = employeeServices.employeeCount();
+        log.info("Employee count fetched: {}", employeeCount);
+        return employeeCount;
     }
 
     @GetMapping("/get_employees")
-    public ResponseEntity<List<EmployeeDTO>> getEmployees() {
-        List<EmployeeDTO> employees = employeeServices.getEmployees();
+    public ResponseEntity<List<ResponseEmployeeDTO>> getEmployees() {
+        log.info("Fetching employees");
+        List<ResponseEmployeeDTO> employees = employeeServices.getEmployees();
+        log.info("Employees fetched");
         return ResponseEntity.ok(employees);
     }
 }
