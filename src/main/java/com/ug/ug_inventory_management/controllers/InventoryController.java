191db5a7a543e530e35428f3c5bbd7ce15468530
shivsharanprakash <shivsharanp49@gmail.com>
2026-04-02 23:40:21 +0530

    extract user details from Authentication in inventory update

--- src/main/java/com/ug/ug_inventory_management/controllers/InventoryController.java
+++ src/main/java/com/ug/ug_inventory_management/controllers/InventoryController.java
@@ -1,10 +1,13 @@
 package com.ug.ug_inventory_management.controllers;
-
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.GrantedAuthority;
 import com.ug.ug_inventory_management.services.InventoryService;
 import com.ug.ug_inventory_management.common.dtos.CreateInventoryRecordDTO;
 import com.ug.ug_inventory_management.common.dtos.InventoryUpdateRequest;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
+import com.ug.ug_inventory_management.common.security.CustomUserDetails;
 
 import java.util.List;
 import java.util.Map;
@@ -39,10 +42,48 @@ public class InventoryController {
         return inventoryService.getInventorySummary(templateId);
     }
 
-
+//--------------Testing Controller -------///
 
     @PostMapping("/update")
-    public ResponseEntity<?> updateInventory(@RequestBody InventoryUpdateRequest request) {
-        return inventoryService.updateInventory(request);
+    public ResponseEntity<?> updateInventory(
+            @RequestBody InventoryUpdateRequest req,
+            Authentication auth
+    ) {
+
+        Long eId;
+        String unitName;
+        String role = "EMPLOYEE";
+
+        if (auth != null && auth.getPrincipal() instanceof CustomUserDetails user) {
+            eId = user.getEId();
+            unitName = user.getUnitName();
+        } else {
+            eId = 1L;
+            unitName = "UNIT_1";
+        }
+
+        return inventoryService.updateInventory(req, role, eId, unitName);
     }
+
+
+    //--------------Authenticated User only make changes in the qty ----------//
+//    @PostMapping("/update")
+//    public ResponseEntity<?> updateInventory(
+//            @RequestBody InventoryUpdateRequest req,
+//            Authentication auth
+//    ) {
+//        Long eId = null;
+//        String unitName = null;
+//        String role = "EMPLOYEE";
+//
+//        if (auth != null && auth.getPrincipal() instanceof CustomUserDetails user) {
+//            eId = user.getEId();
+//            unitName = user.getUnitName();
+//            role = user.getAuthorities().iterator().next().getAuthority();
+//        } else {
+//            return ResponseEntity.status(401).body("User not authenticated");
+//        }
+//
+//        return inventoryService.updateInventory(req, role, eId, unitName);
+//    }
 }
