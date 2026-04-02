325fa9e41e42e9f4a554552be5f624946e8307cb
shivsharanprakash <shivsharanp49@gmail.com>
2026-04-02 23:41:42 +0530

    pdate InventoryLog to store performedBy as user id

--- src/main/java/com/ug/ug_inventory_management/models/InventoryLog.java
+++ src/main/java/com/ug/ug_inventory_management/models/InventoryLog.java
@@ -1,4 +1,44 @@
 package com.ug.ug_inventory_management.models;
 
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+@Entity
+@Table(name = "inventory_audit_logs")
 public class InventoryLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long templateId;
+
+    private String unitName;
+
+    private String action;
+
+    private Integer changeQty;
+    private Integer previousQty;
+    private Integer newQty;
+
+    @Column(name = "performed_by")
+    private Long performedBy;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public InventoryLog() {}
+
+    public InventoryLog(Long templateId, String unitName, String action,
+                        Integer changeQty, Integer previousQty,
+                        Integer newQty, Long performedBy) {
+        this.templateId = templateId;
+        this.unitName = unitName;
+        this.action = action;
+        this.changeQty = changeQty;
+        this.previousQty = previousQty;
+        this.newQty = newQty;
+        this.performedBy = performedBy;
+    }
+
 }
+
