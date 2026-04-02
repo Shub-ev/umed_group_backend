a1cf85e8467adb0d846055cd65ac4089a8c62e12
shivsharanprakash <shivsharanp49@gmail.com>
2026-04-02 23:44:02 +0530

    ntroduce InventoryAuditEvent for async logging

--- src/main/java/com/ug/ug_inventory_management/common/events/InventoryAuditEvent.java
+++ src/main/java/com/ug/ug_inventory_management/common/events/InventoryAuditEvent.java
@@ -1,4 +1,16 @@
 package com.ug.ug_inventory_management.common.events;
 
+import com.ug.ug_inventory_management.models.InventoryLog;
+
 public class InventoryAuditEvent {
+
+    private final InventoryLog log;
+
+    public InventoryAuditEvent(InventoryLog log) {
+        this.log = log;
+    }
+
+    public InventoryLog getLog() {
+        return log;
+    }
 }
