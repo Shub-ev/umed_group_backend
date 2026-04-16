986827b6ec86b1be33550fcaa3f36a15433ba894
Shub-ev <shubhamg200204@gmail.com>
2026-04-16 13:15:26 +0530

    add findByPreformedByOrderByCreatedAtDesc method

--- src/main/java/com/ug/ug_inventory_management/repositories/InventoryLogRepository.java
+++ src/main/java/com/ug/ug_inventory_management/repositories/InventoryLogRepository.java
@@ -4,17 +4,13 @@ import com.ug.ug_inventory_management.models.InventoryLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-//public interface InventoryLogRepository extends JpaRepository<InventoryLog, Long> {
-//    List<InventoryLog> findByUnitNameOrderByCreatedAtDesc(String unitName);
-//    List<InventoryLog> findByUnitNameAndTemplateIdOrderByCreatedAtDesc(String unitName, Long templateId);
-//}
-
 //@Repository
 public interface InventoryLogRepository extends JpaRepository<InventoryLog, Long> {
-
     List<InventoryLog> findByUnitNameOrderByCreatedAtDesc(String unitName);
 
     List<InventoryLog> findByUnitNameAndTemplateIdOrderByCreatedAtDesc(String unitName, Long templateId);
+
+    List<InventoryLog> findByPerformedByOrderByCreatedAtDesc(Long eId);
 
     @Query("SELECT DISTINCT r.unitName FROM InventoryRecord r")
     List<String> findAllUnits();
