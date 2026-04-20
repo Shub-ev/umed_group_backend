09f84c3b899cab610ae267922754f802304573e2
Shub-ev <shubhamg200204@gmail.com>
2026-04-20 22:46:29 +0530

    create repository corresponding to UnitName Entity

---
+++ src/main/java/com/ug/ug_inventory_management/repositories/UnitNameRepository.java
@@ -0,0 +1,10 @@
+package com.ug.ug_inventory_management.repositories;
+
+import com.ug.ug_inventory_management.models.UnitName;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UnitNameRepository extends JpaRepository<UnitName, String> {
+    Optional<UnitName> findByUnitName(String unitName);
+}
