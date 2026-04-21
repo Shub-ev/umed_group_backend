3335bc63906bdcc700e18d6232c7d9a901c9a6f1
Shivsharanprakash <shivsharanp49@gmail.com>
2026-04-21 21:05:46 +0530

    Add stock alert DTO

---
+++ src/main/java/com/ug/ug_inventory_management/common/dtos/StockAlertDTO.java
@@ -0,0 +1,41 @@
+package com.ug.ug_inventory_management.common.dtos;
+
+public class StockAlertDTO {
+
+    private String unitName;
+    private String templateName;
+    private int stock;
+
+    public StockAlertDTO() {
+    }
+
+    public StockAlertDTO(String unitName, String templateName, int stock) {
+        this.unitName = unitName;
+        this.templateName = templateName;
+        this.stock = stock;
+    }
+
+    public String getUnitName() {
+        return unitName;
+    }
+
+    public void setUnitName(String unitName) {
+        this.unitName = unitName;
+    }
+
+    public String getTemplateName() {
+        return templateName;
+    }
+
+    public void setTemplateName(String templateName) {
+        this.templateName = templateName;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
