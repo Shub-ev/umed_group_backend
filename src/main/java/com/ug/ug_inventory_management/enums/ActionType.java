384e35d165be3e584423a5536248a29f4ed0e029
Shub-ev <shubhamg200204@gmail.com>
2026-04-22 19:02:15 +0530

    add CREATE action

--- src/main/java/com/ug/ug_inventory_management/enums/ActionType.java
+++ src/main/java/com/ug/ug_inventory_management/enums/ActionType.java
@@ -2,7 +2,8 @@ package com.ug.ug_inventory_management.enums;
 import com.fasterxml.jackson.annotation.JsonCreator;
 public enum ActionType {
     INWARD,
-    OUTWARD;
+    OUTWARD,
+    CREATE;
     @JsonCreator
     public static ActionType fromString(String value) {
         return ActionType.valueOf(value.toUpperCase());
