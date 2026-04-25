1a9de1610e37b92a8713de63676698826e14427b
Shivsharanprakash <shivsharanp49@gmail.com>
2026-04-25 23:47:18 +0530

    update repository to get cumulitive sum .

--- src/main/java/online/umedgroup/ug_inventory_management/repositories/ReportRepository.java
+++ src/main/java/online/umedgroup/ug_inventory_management/repositories/ReportRepository.java
@@ -11,38 +11,40 @@ import java.util.List;
 public interface ReportRepository extends JpaRepository<InventoryLog, Long> {
 
     @Query(value = """
-    SELECT 
-        l.unit_name,
-        t.id,
-        t.template_name,
-        l.main_field_value,
+SELECT 
+    l.unit_name,
+    t.id,
+    t.template_name,
+    l.main_field_value,
 
-        SUM(CASE WHEN l.action = 'INWARD' THEN l.change_qty ELSE 0 END),
-        SUM(CASE WHEN l.action = 'OUTWARD' THEN l.change_qty ELSE 0 END),
-        SUM(CASE 
-            WHEN l.action = 'INWARD' THEN l.change_qty 
-            WHEN l.action = 'OUTWARD' THEN -l.change_qty 
-            ELSE 0 
-        END)
+    SUM(CASE WHEN l.action = 'INWARD' THEN l.change_qty ELSE 0 END),
+    SUM(CASE WHEN l.action = 'OUTWARD' THEN l.change_qty ELSE 0 END),
+    SUM(CASE 
+        WHEN l.action = 'INWARD' THEN l.change_qty 
+        WHEN l.action = 'OUTWARD' THEN -l.change_qty 
+        ELSE 0 
+    END)
 
-    FROM inventory_audit_logs l
-    JOIN templates t ON t.id = l.template_id
-    WHERE 
-        (:fromDate IS NULL OR l.created_at >= :fromDate)
-        AND (:toDate IS NULL OR l.created_at <= :toDate)
-        AND (:unit IS NULL OR LOWER(l.unit_name) LIKE LOWER(CONCAT('%', :unit, '%')))
-        AND (:templateId IS NULL OR t.id = :templateId)
+FROM inventory_audit_logs l
+JOIN templates t ON t.id = l.template_id
+WHERE 
+    (:fromDate IS NULL OR l.created_at >= :fromDate)
+    AND (:toDate IS NULL OR l.created_at <= :toDate)
+    AND (:unit IS NULL OR LOWER(l.unit_name) LIKE LOWER(CONCAT('%', :unit, '%')))
+    AND (:mainField IS NULL OR LOWER(l.main_field_value) LIKE LOWER(CONCAT('%', :mainField, '%')))
+    AND (:templateId IS NULL OR t.id = :templateId)
 
-    GROUP BY 
-        l.unit_name, 
-        t.id, 
-        t.template_name,
-        l.main_field_value
+GROUP BY 
+    l.unit_name, 
+    t.id, 
+    t.template_name,
+    l.main_field_value
 """, nativeQuery = true)
     List<Object[]> getMainFieldReport(
             @Param("fromDate") LocalDateTime fromDate,
             @Param("toDate") LocalDateTime toDate,
             @Param("unit") String unit,
+            @Param("mainField") String mainField,
             @Param("templateId") Long templateId
     );
 
