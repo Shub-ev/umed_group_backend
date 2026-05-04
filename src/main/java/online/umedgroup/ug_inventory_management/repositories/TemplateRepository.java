3a2f1e5ab28ca682917bc743770bc59ed131e3c8
shubham <shubham@shub-kali.shub>
2026-05-04 20:24:50 +0530

    add new methods to get templates based on roles

--- src/main/java/online/umedgroup/ug_inventory_management/repositories/TemplateRepository.java
+++ src/main/java/online/umedgroup/ug_inventory_management/repositories/TemplateRepository.java
@@ -3,6 +3,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import online.umedgroup.ug_inventory_management.models.Template;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.Optional;
 
 
@@ -15,4 +18,27 @@ public interface TemplateRepository extends JpaRepository<Template, Long> {
     Page<Template> findByTemplateNameContainingIgnoreCase(String templateName, Pageable pageable);
 
     Optional<Template> findById(Long id);
+
+    @Query("""
+        SELECT t FROM Template t
+        LEFT JOIN t.employees e
+        WHERE t.isRestricted = false
+        OR e.id = :employeeId
+       """)
+    Page<Template> findEmployeeAccessibleTemplates(
+            @Param("employeeId") Long employeeId,
+            Pageable pageable
+    );
+
+    @Query("""
+        SELECT t FROM Template t
+        LEFT JOIN t.employees e
+        WHERE (t.isRestricted = false OR e.id = :employeeId)
+        AND LOWER(t.templateName) LIKE LOWER(CONCAT('%', :name, '%'))
+    """)
+    Page<Template> findEmployeeAccessibleTemplatesByName(
+            @Param("employeeId") Long employeeId,
+            @Param("name") String name,
+            Pageable pageable
+    );
 }
